f4cf701b0bcc6ade9a6e285c6a7e520259d403b7
Vaibhav <vaibhav.sharma3100@gmail.com>
2025-02-15 19:27:42 +0530

    FE-updates

--- ChatApp/src/App.tsx
+++ ChatApp/src/App.tsx
@@ -5,9 +5,11 @@ function App() {
 
   return (
     <div className='h-screen bg-red-500'>
-      <div className='h-screen bg-black'>
-     hi there
-    </div>
+      <div className='h-[90ww] bg-red-300'>
+        <div className=''>
+          <input type="text"></input>
+        </div>
+      </div>
     </div>
   )
 }
