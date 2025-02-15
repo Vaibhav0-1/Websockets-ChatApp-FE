dce8a49d177f916f3d8156309e63c02e93ea5cc7
Vaibhav <vaibhav.sharma3100@gmail.com>
2025-02-15 19:58:00 +0530

    ChatApp design FE

--- ChatApp/src/App.tsx
+++ ChatApp/src/App.tsx
@@ -1,17 +1,20 @@
 import './App.css'
 
 function App() {
-  
-
   return (
-    <div className='h-screen bg-red-500'>
-      <div className='h-[90ww] bg-red-300'>
-        <div className=''>
-          <input type="text"></input>
-        </div>
+    <div className='h-screen bg-black'>
+      <div className='h-[95vh]'></div>
+      <div className='flex w-full items-center gap-2  bg-white p-4'>
+        <input 
+          placeholder='Message' 
+          type="text" 
+          className="w-full flex bg-gray-700 text-white rounded-lg p-4 focus:outline-none"
+        />
+        <button className="bg-purple-600 text-white p-4 rounded-lg hover:bg-purple-700">
+          Send Message
+        </button>
       </div>
     </div>
   )
 }
-
 export default App
